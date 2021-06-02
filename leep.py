db61b9111a6b7860dfc1a4e9a265e3a514eb4967
Simon Fischer <github@simon-fischer.info>
2021-06-03 01:04:22 +0200

    added mirror domains, improved title

--- leep.py
+++ leep.py
@@ -13,21 +13,31 @@ import copy
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import colors
+import matplotlib as mpl
 
+mpl.rcParams.update({
+    "font.family": "sans-serif", 
+    "font.sans-serif": ["Ubuntu"],
+    "font.size": 12,
+    "axes.labelsize": "small",
+    "axes.titlesize": "small",
+    "figure.titlesize": "small",
+    "text.usetex": True,
+    "text.latex.preamble": r"\usepackage{mathtools} \usepackage{sfmath} \usepackage{siunitx}"
+})
 
 DEFAULTS = {
     "hex": [[1, 0], [np.cos(2 * np.pi / 3), np.sin(2 * np.pi / 3)]],
     "square": [[1, 0], [0, 1]],
     "rect": [[1, 0], [0, 2]],
     "centered": [[1, 0], [0.5, 0.3]],
-    "monoclinic": [[1, 0], [0.2, 1.3]],
+    "oblique": [[1, 0], [0.2, 1.3]],
 
     "ru": [[2.71, 0], [2.71 * np.cos(2 * np.pi / 3), 2.71 * np.sin(2 * np.pi / 3)]],
     "ruo2-110": [[3.11, 0], [0, 6.38]],
     "ruo2-100": [[3.11, 0], [0, 4.49]],
-    "vo2-100": [[0, -2.8514], [4.5546, 0]],
-    "vo2-110": [[0, -2.8514], [4.5546 * np.sqrt(2), 0]]
+    "vo2-100": [[2.8514, 0], [0, 4.5546]],
+    "vo2-110": [[2.8514, 0], [0, 4.5546 * np.sqrt(2)]]
 }
 
 
@@ -42,7 +52,7 @@ def main():
     phases = []
 
     if args.special:
-        uc_ru = UC(*DEFAULTS["ru"], s=1)
+        uc_ru = UC(*DEFAULTS["ru"], s=1, label="substrate")
         phases.append(uc_ru)
         if "110" in args.special[0].lower():
             if "ruo2" in args.special[0].lower():
@@ -52,50 +62,74 @@ def main():
         if "100" in args.special[0].lower():
             if "ruo2" in args.special[0].lower():
                 phases.append(UC(*DEFAULTS["ruo2-100"], parent=uc_ru, color="b", s=0.6))
+#                phases.append(UC(*DEFAULTS["ruo2-100"], theta=4, parent=uc_ru, color="b", s=0.6))
+#                phases.append(UC(*DEFAULTS["ruo2-100"], theta=-4, parent=uc_ru, color="b", s=0.6))
             if "vo2" in args.special[0].lower():
                 phases.append(UC(*DEFAULTS["vo2-100"], parent=uc_ru, color="b", s=0.6))
         for phase in phases[1:].copy():
-            for color, theta in zip(("g", "r"), (2 * np.pi / 3, 4 * np.pi / 3)):
+            for color, theta in zip(("y", "c"), (2 * np.pi / 3, 4 * np.pi / 3)):
                 phases.append(phase.get_reconstruction(theta=theta, color=color, label="rot_dom"))
-        plot_phases(phases, title=args.special[0])
+        plot_phases(phases, title=args.special[0], show_all=args.showall)
         plt.show()
         return
 
     if args.vectors is not None:
         # if the unit vectors are given in the command line, use them to make the UC
         uc = UC(args.vectors[:2], args.vectors[2:], label="substrate")
-        title += "a1=" + str(args.vectors[:2]) + ", a2=" + str(args.vectors[2:])
     else:
         # else, use one of the bases defined in DEFAULTS
         uc = UC(*DEFAULTS[args.base[0]], label="substrate")
-        title += args.base[0]
+        title += args.base[0] + " "
+    title += "base: " + format_base(uc.real_base)
+
     # add the UC to the list of phases
     phases.append(uc)
 
     # calculate the list of rotation angles to use from the number of rotational domains
-    thetas = np.linspace(0, 2 * np.pi, args.rotations[0] + 1)[1:-1]
+    thetas = np.linspace(0, 2 * np.pi, args.rotations[0] + 1)   #[1:-1]
+    
+    if args.rotations[0] == 4:
+        thetas = thetas[:2]
 
     # parse the "matrix" argument into actual matrices
     matrices = list([matrix[:2], matrix[2:]] for matrix in args.matrix)
 
-    for rec in args.woods + matrices:
-        # make a reconstruction unit cell from rec, which is either a woods
-        # notation or a matrix
-        reconstruction = uc.get_reconstruction(rec, color="b", s=0.6, label="rec")
-        title += " | " + str(rec) + " reconstr."
-        # add the reconstruction to the list of phases
-        phases.append(reconstruction)
-        for theta in thetas:
-            # if rotational domains are required, add them
-            rot_dom = reconstruction.get_reconstruction(theta=theta, label="rot_dom")
-            phases.append(rot_dom)
 
-    if args.rotations[0] > 1:
-        title += " | " + str(args.rotations[0]) + " rot. dom."
+    colors = ("b", "g", "y", "c", "m")
+    recs = args.woods + matrices
+    for rec, color in zip(recs, colors[:len(recs)]):
+        # make a reconstruction unit cell from rec, which is either a woods notation or a matrix
+        title += ", reconstruction: " + format_woods(rec)
+        # add the reconstruction to the list of phases
+        label = "rec"
+        # go through the different rotations
+        for theta in thetas:
+            # only the first domain is called rec and will be plotted in real space
+            if theta > 0:
+                label = "rot_dom"
+            # add rotational domains
+            rot_dom = uc.get_reconstruction(
+                rec, theta=theta, 
+                color=color, s=0.6, label=label
+            )
+            phases.append(rot_dom)
+            # add mirror domains if necessary
+            if args.mirror:
+                mirror_dom = uc.get_reconstruction(
+                    rec, theta=theta, 
+                    color=color, s=0.6, mirror=True, label="mirror_dom"
+                )
+                phases.append(mirror_dom)
+
+    if args.rotations[0] > 2:
+        title += " and " + str(args.rotations[0]) + " rotational domains"
+    if args.mirror:
+        title += " and a mirror domain"
 
     # do the plotting
-    plot_phases(phases, title=title)
+    plot_phases(phases, title=title, show_all=args.showall)
 
+#    plt.subplots_adjust(top=0.8)
     if args.output:
         # save an image if the argument is given
         plt.savefig(args.output)
@@ -116,7 +150,7 @@ def parse_args(arglist):
     base_group.add_argument(
         "-b", "--base", nargs=1, default=["square"], type=str,
         help="Real space base. Either one of 'hex,square,rect,centered,"
-             "monoclinic'.", metavar="BASE")
+             "oblique'.", metavar="BASE")
     base_group.add_argument(
         "-v", "--vectors", nargs=4, type=float,
         help="Explicit base vectors explicitly (instead of 'BASE').",
@@ -135,28 +169,37 @@ def parse_args(arglist):
         metavar=("A11", "A12", "A21", "A22"))
     parser.add_argument(
         "-r", "--rotations", type=int, nargs=1, default=[1],
-        help="Number of rotational domains. Applies to all defined reconstructions.")
+        help="Number of rotational domains. Applies to all defined reconstructions."
+             "If r is 2, the domain is mirrored at the h=k=1 direction")
+    parser.add_argument(
+        "-p", "--mirror", action="store_true", help="Show mirror domains.")
     parser.add_argument(
         "-o", "--output", type=str, help="Save image file.",
         metavar="fname")
+    parser.add_argument(
+        "-a", "--showall", action="store_true", help="show all domains in real space")
     args = parser.parse_args(arglist)
     return args
 
 
-def plot_phases(phases, title=""):
+def plot_phases(phases, title="", show_all=False):
     # make 2 axes with a 1:1 aspect ratio
-    fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
     for ax in axes.flatten():
         ax.set_aspect("equal", adjustable="datalim")
     fig.suptitle(title)
 
     # plot each phases' base and pattern in reciprocal and in real space
     for phase in phases:
-        if not "rot_dom" in phase.label:
+        if "substrate" in phase.label:
+            color = "k"
+        else:
+            color = phase.color
+        if show_all or not "dom" in phase.label:
+            phase.plot_base(ax=axes[0], color=color)
             phase.plot_pattern(ax=axes[0])
-            phase.plot_base(ax=axes[0])
+        phase.plot_base(rec=True, ax=axes[1], color=color)
         phase.plot_pattern(rec=True, ax=axes[1])
-        phase.plot_base(rec=True, ax=axes[1])
 
 
 class UC:
@@ -170,14 +213,21 @@ class UC:
     If a "parent" unit cell is given, its size can be used for the axes dimensions when plotting.
     The color and size of the pattern spots are given by color and s.
     """
-    def __init__(self, a1, a2, a3=None, parent=None, color="k", s=1, label=""):
+    def __init__(self, a1, a2, a3=None, theta=None, parent=None, color="r", s=1, label="", M=None):
         if a3 is None:
             a3 = np.array([0, 0, 1])
         a1 = _make_vector_3d(a1)
         a2 = _make_vector_3d(a2)
         a3 = _make_vector_3d(a3)
+        
+        if M is None:
+            M = np.eye(3)
+        self.M = M
 
         self._real_base = np.array([a1, a2, a3]).transpose()
+        if theta is not None:
+            theta *= np.pi / 180
+            self._real_base = self._apply_rotation(self._real_base, theta)
         self._rec_base = np.linalg.inv(self.real_base).transpose() * 2 * np.pi
 
         self.label = label
@@ -232,7 +282,7 @@ class UC:
             return np.dot(R, base)
         return base
 
-    def get_reconstruction(self, M="1x1", theta=0, **kwargs):
+    def get_reconstruction(self, M="1x1", theta=0, mirror=False, **kwargs):
         """Returns a "child" unit cell with the base transformed by M.
         M can either be a reconstruction matrix or a woods notation.
         """
@@ -241,6 +291,9 @@ class UC:
         # if M is a string, this will make it into a transformation matrix.
         # also, 2x2 matrices are expanded into 3x3 matrices
         M, theta = _parse_woods(M, theta)
+        if mirror:
+            M = M[:, (1, 0, 2)]     # swap first two columns
+            theta = -theta
         # first the matrix, then the rotation is applied
         base = self._apply_matrix(self.real_base, M)
         base = self._apply_rotation(base, theta)
@@ -250,33 +303,36 @@ class UC:
         # if color and s are given in kwargs, they override the default
         sub_kwargs.update(kwargs)
 
-        cell = UC(*base.transpose(), parent=self.parent, **sub_kwargs)
+        cell = UC(*base.transpose(), parent=self.parent, M=M, **sub_kwargs)
         return cell
 
-    def plot_base(self, rec=False, ax=None):
+    def plot_base(self, color=None, rec=False, ax=None):
         """Plots the base vectors as arrows, either in reciprocal or real space."""
         assert self.is_2d
         if rec:
             base = self.rec_base
         else:
             base = self.real_base
+        if color is None:
+            color = self.color
 
         if ax is None:
             _, ax = plt.subplots()
             ax.set_aspect("equal")
+            
+        s00 = np.array([0, 0])
+        s10 = np.array([base[0, 0], base[1, 0]])
+        s01 = np.array([base[0, 1], base[1, 1]])
+        elongation = 1.015
 
-        ax.annotate(
-            "", xy=(base[0, 0], base[1, 0]), xytext=(0, 0),
-            arrowprops=dict(arrowstyle="-|>, head_width=0.4, head_length=1", color=self.color))
-        ax.annotate(
-            "", xy=(base[0, 1], base[1, 1]), xytext=(0, 0),
-            arrowprops=dict(arrowstyle="-|>, head_width=0.4, head_length=1", color=self.color))
-        ax.annotate(
-            "", xy=(base[0, 0] + base[0, 1], base[1, 0] + base[1, 1]), xytext=(base[0, 0], base[1, 0]),
-            arrowprops=dict(arrowstyle="-", linestyle="--", color=self.color))
-        ax.annotate(
-            "", xy=(base[0, 0] + base[0, 1], base[1, 0] + base[1, 1]), xytext=(base[0, 1], base[1, 1]),
-            arrowprops=dict(arrowstyle="-", linestyle="--", color=self.color))
+        ax.annotate("", xy=s10 * elongation, xytext=s00,
+                    arrowprops=dict(arrowstyle="-|>, head_width=0.4, head_length=1", color=color))
+        ax.annotate("", xy=s01 * elongation, xytext=s00,
+                    arrowprops=dict(arrowstyle="-|>, head_width=0.4, head_length=1", color=color))
+        ax.annotate("", xy=s01 * elongation + s10, xytext=s10,
+                    arrowprops=dict(arrowstyle="-", linestyle=(0, (3, 4)), color=color))
+        ax.annotate("", xy=s01 + s10 * elongation, xytext=s01,
+                    arrowprops=dict(arrowstyle="-", linestyle=(0, (3, 4)), color=color))
         return ax
 
     def plot_pattern(self, rec=False, ax=None, ucs=None, d=None):
@@ -327,10 +383,10 @@ class UC:
 
         if rec:
             ax.scatter(points[:, 0], points[:, 1], color=self.color, s=self.s**3 * 250 / ucs)
-            ax.set_title("Reciprocal space")
+            ax.set_title("Reciprocal space", y=-0.12)
         else:
             gauplot([(x, y) for x, y, _ in points], self.s * d, size=size*1.2, ax=ax, color=self.color)
-            ax.set_title("Real space")
+            ax.set_title("Real space", y=-0.12)
         return ax
 
     @property
@@ -341,11 +397,11 @@ class UC:
         return self._rec_base
 
 
-def gauplot(centers, radius=5, size=20, ax=None, color="r", thresh=2):
+def gauplot(centers, radius=5, size=20, ax=None, color="r", thresh=3):
     """adapted from https://stackoverflow.com/questions/10958835"""
-    color = colors.to_rgb(color)
+    color = mpl.colors.to_rgb(color)
     cmap_mat = np.array([np.linspace(c, 1, 256) for c in color]).T
-    cmap = colors.ListedColormap(cmap_mat)
+    cmap = mpl.colors.ListedColormap(cmap_mat)
     cmap.set_bad(alpha=0)
     
     nx, ny = 1000.,1000.
@@ -365,6 +421,55 @@ def _make_vector_3d(x, third=0):
         x = np.array([*x, third])
     assert len(x) == 3
     return x
+    
+def format_base(base, dim=2):
+    if dim != 2:
+        raise NotImplementedError
+    def vec2str(vec):
+        return (r"\begin{pmatrix*}[c]"
+                + f"{vec[0]:.2f}".rstrip("0").rstrip(".") + r" \\ " 
+                + f"{vec[1]:.2f}".rstrip("0").rstrip(".")
+                + r"\end{pmatrix*}")
+    return "$a_1 = " + vec2str(base[:2, 0]) + ", a_2 =" + vec2str(base[:2, 1]) + "$"
+    
+    
+def format_woods(M):
+    outstr = ""
+    if isinstance(M, str):
+        if M.startswith("c"):
+            outstr += "c"
+        elif M.startswith("p"):
+            outstr += "p"
+        outstr += r"$\left("
+
+        a = re.findall("([\.sqrt0-9]*)\s*x", M)[0]
+        a0 = int(re.findall("\d+\.*\d*", a)[0])
+        if any(s in a for s in ("s", "r", "sqrt")):
+            outstr += r"\sqrt{" + str(int(a0)) + r"} \times"
+        else:
+            outstr += str(int(a0)) + r" \times"
+
+        b = re.findall("x\s*([\.sqrt0-9]*)", M)[0]
+        b0 = float(re.findall("\d+\.*\d*", b)[0])
+        if any(s in b for s in ("s", "r", "sqrt")):
+            outstr += r"\sqrt{" + str(int(b0)) + r"}"
+        else:
+            outstr += str(int(b0))
+        outstr += r"\right)$"
+
+        t = re.findall("[Rr]\s*(\d+\.*\d*)", M)
+        if t:
+            outstr += r"R\ang{" + str(float(t[-1])).rstrip("0").rstrip(".") + r"}"
+    else:
+        M = np.array(M)
+        outstr += (
+            r"$\begin{pmatrix*}[c]"
+            + str(M[0, 0]) + " & " + str(M[0, 1]) + r" \\ "
+            + str(M[1, 0]) + " & " + str(M[1, 1])
+            + r"\end{pmatrix*}$"
+        )
+    return outstr
+
 
 def _parse_woods(M, theta=0):
     """Parse a woods notation string into a 3x3 transformation matrix."""
@@ -383,7 +488,7 @@ def _parse_woods(M, theta=0):
         else:
             b = b0
 
-        t = re.findall("[Rr]\s*([-0-9]*)", M)
+        t = re.findall("[Rr]\s*(\d+\.*\d*)", M)
         if t:
             theta = np.pi * float(t[-1]) / 180
 
@@ -393,10 +498,15 @@ def _parse_woods(M, theta=0):
             M = np.array([[a, 0], [1, 1]])
         else:
             M = np.array([[a, 0], [0, b]])
-
-    M = np.pad(M, ((0, 1), (0, 1)), constant_values=0)
-    M[2, 2] = 1
+    
+    M = np.array(M)
+    if M.shape == (2, 2):
+        M = np.pad(M, ((0, 1), (0, 1)), constant_values=0)
+        M[2, 2] = 1
+    if M.shape != (3, 3):
+        raise ValueError("Could not convert to 3x3 matrix")
     return M, theta
+
 
 def norm(x):
     """Return the euclidian length of a vector"""
